58aa13e8d6c7d66f09f44cd1affebd4c9978d443
Muhammet Tuna <mahotuna6@gmail.com>
2023-10-26 17:01:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
-import axios from "axios";
+
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-
+    res.render("index.ejs");
 })
 
 app.listen(port, () => {
-    console.log('server is listening on port ${port}')
-})
+    console.log(`Server is running on port ${port}`);
+});
